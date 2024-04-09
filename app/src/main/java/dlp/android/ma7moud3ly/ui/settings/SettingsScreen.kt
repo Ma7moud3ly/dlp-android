d629d7ccbc953740bcf0011a8f851f33a6e65f0d
ma7moud3ly <m.aly@gasable.com>
2024-04-10 00:52:36 +0200

    - version 1.0

---
+++ app/src/main/java/dlp/android/ma7moud3ly/ui/settings/SettingsScreen.kt
@@ -0,0 +1,29 @@
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import dlp.android.ma7moud3ly.managers.DownloadManager
+
+
+@Composable
+fun SettingsScreen() {
+    val activity = LocalContext.current as Activity
+    val downloadManager = remember { DownloadManager.instance }
+    val dlpVersion = remember { downloadManager.dlpVersion() }
+
+
+    SettingsScreenContent(
+        dlpVersion = dlpVersion,
+        onOpenRepo = {
+            try {
+                val url = "https://github.com/Ma7moud3ly/dlp-android"
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                activity.startActivity(browserIntent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    )
+}
